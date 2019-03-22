ad8f6ca5ffff56766b6e997ffc136be873f854f6
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2019-03-22 13:37:56 +0100

    pylint

--- web_responsive/__init__.py
+++ web_responsive/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018  Alexandre Díaz
+# Copyright 2018 Alexandre Díaz
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from . import models
--- web_responsive/models/__init__.py
+++ web_responsive/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018  Alexandre Díaz
+# Copyright 2018 Alexandre Díaz
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import inherited_res_users
